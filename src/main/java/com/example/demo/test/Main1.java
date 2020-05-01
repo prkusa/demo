b0f5c91bb019f3b7830ba8f51bef57540c9c9838
prkusa <prk.usa@gmail.com>
2020-05-01 08:30:45 -0400

    my commit to gama

---
+++ src/main/java/com/example/demo/test/Main1.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+public class Main1 {
+
+	public Main1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("in gama branch");
+	}
+
+}
